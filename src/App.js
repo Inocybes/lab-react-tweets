63c412aecd98647d5464b046b08945d29ae799ad
Eduard Cabrero <eduardc.herrero@gmail.com>
2022-03-07 18:36:00 +0100

    Finished

--- src/App.js
+++ src/App.js
@@ -11,6 +11,8 @@ const tweetsArray = [
     timestamp: "1h ago",
     message:
       "the human likes to say. that i live here rent free. but i would argue. this housing accommodation. is my payment. for a lifetime of love. and excellent company",
+
+      
   },
   {
     user: {
@@ -37,9 +39,16 @@ const tweetsArray = [
 function App() {
   return (
     <div className="App">
-      <Tweet />
+      <Tweet tweet={ tweetsArray[0] }/>
+      
+      
+
+      <Tweet tweet={ tweetsArray[1] }/>
+      <Tweet tweet={ tweetsArray[2] }/>
     </div>
   );
 }
+
+
 
 export default App;
